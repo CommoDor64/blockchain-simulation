61940c349dc4f8ab47494f84aa59a2b98bea3798
Dor Cohen <commo64dor@gmail.com>
2019-05-07 01:27:10 +0200

    documentation fix

--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var Controller = require('../controllers/Controller');
 var controller = new Controller();
 var initValidationMiddleware = require('../middlewares/initValidation');
-var balanceValidationMiddleware = require('../middlewares/balanceValidation'); 
-//middleware
+var balanceValidationMiddleware = require('../middlewares/balanceValidation');
+ 
 router.get('/', (req, res, next) => controller.index(req, res, next));
 router.get('/balance/:id', (req, res, next) => balanceValidationMiddleware(req, res, next), (req, res, next) => controller.balance(req, res, next))
 router.post('/init',(req, res, next) => initValidationMiddleware(req, res, next) ,(req, res, next) => controller.init(req, res, next));
